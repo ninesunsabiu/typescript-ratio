3c581fa75612bcfae63006fba42e850b06370ecc
Ezio Lin <ninesuns.lin@outlook.com>
2020-05-17 10:40:56 +0800

    add: script and deno config

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 为了查看使用 Typescript 在项目中的占比 有了这个小脚本  
 而且 赶上 deno 1.0 的出现，试试用它来写是个什么感觉
 
+## 用法
+1. 安装 deno [详见官网安装教程](https://deno.land/#installation)
+2. $ deno run --allow-read tsRatio.ts ${DIR_PATH}  
+*${DIR_PATH} 更换为你需要统计的目录 一般为项目中 src 目录*
+3. 你可以看见
+```bash
+$ deno run --allow-read tsRatio.ts ./src
+Typescript rate is 41%, Javascript count is 1540, Typescript count is 1090
+```
---
+++ tsRatio.ts
@@ -0,0 +1,38 @@
+import { walk, WalkOptions } from "https://deno.land/std@v0.51.0/fs/walk.ts";
+
+const walkOptions: WalkOptions = {
+  match: [/\.[jt]sx?$/],
+  skip: [
+    /node_modules|public|script/,
+    /config\/(jest\/|.*\.js)/,
+    /webpack\.config/,
+  ],
+};
+
+async function tsRatio() {
+  let tsCount = 0;
+  let jsCount = 0;
+  try {
+    const dirPath = Deno.args[0];
+    if (!(await Deno.lstat(dirPath)).isDirectory) {
+      throw new Error("need directory path as first arg");
+    }
+    for await (const entry of walk(dirPath, walkOptions)) {
+      if (entry.path.match(/\.tsx?$/)) {
+        tsCount++;
+      }
+      jsCount++;
+    }
+    (tsCount + jsCount) &&
+      console.log(
+        "Typescript rate is %o%, Javascript count is %d, Typescript count is %d",
+        Number(Number(tsCount / (jsCount + tsCount)).toFixed(2)) * 100,
+        jsCount,
+        tsCount,
+      );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+tsRatio();
